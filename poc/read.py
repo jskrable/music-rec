875dc3bee6a038163866831d12d85dbe74acbbf8
Jack Skrable <jskrable@bu.edu>
2019-02-21 12:13:17 -0500

    added progress bar

--- poc/read.py
+++ poc/read.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tables
 import glob
 import time
@@ -13,10 +14,16 @@ from pandas import DataFrame, HDFStore
 
 # store = HDFStore('test.h5')
 
-<<<<<<< HEAD
+# Progress bar for cli
+def progress(count, total, suffix=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = '#' * filled_len + '-' * (bar_len - filled_len)
+    sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', suffix))
+    sys.stdout.flush()
 
-=======
->>>>>>> 3de93ddae4ad8209e2b55faa2c029f2b89b9c07e
+
 def get_all_files(basedir, ext='.h5'):
     """
     From a root directory, go through all subdirectories
@@ -34,24 +41,15 @@ def get_all_files(basedir, ext='.h5'):
 # From a list of h5 files, extracts song metadata and creates a dataframe
 def extract_song_data(files):
     allsongs = pd.DataFrame()
-    limit = 1000
     size = len(files)
-<<<<<<< HEAD
-    # limit = size
-    for i in range(0, limit):
-        complete = '[ '+str(round((i/limit)*100, 2))+'% ]'
-        print(complete, 'of files processed', end='\r', flush=True)
-=======
-    limit = size
-    for i in range(0, limit):
-        complete = str(round((i/limit)*100,2))
-        print(complete+'% of files processed', end='\r', flush=True)
->>>>>>> 3de93ddae4ad8209e2b55faa2c029f2b89b9c07e
-        s_hdf = pd.HDFStore(files[i])
+    print(size,'files found.')
+    for i, f in enumerate(files):
+        progress(i, size, 'of files processed')
+        # print(complete, 'of files processed', end='\r', flush=True)
+        s_hdf = pd.HDFStore(f)
         s_df = pd.DataFrame.from_records(s_hdf.root.metadata.songs[:])
         allsongs = allsongs.append(s_df, ignore_index=True)
         s_hdf.close()
-<<<<<<< HEAD
 
     return allsongs
 
@@ -64,25 +62,19 @@ def convert_byte_data(df):
         df[col] = str_df[col]
 
     return df
-=======
-
-    return allsongs
->>>>>>> 3de93ddae4ad8209e2b55faa2c029f2b89b9c07e
 
 
 # Main
 t1 = time.time()
+print('Getting list of all h5 files...')
 files = get_all_files('./MillionSongSubset/data', '.h5')
 t2 = time.time()
 songsDF = extract_song_data(files)
 t3 = time.time()
 
-print('Got', len(songsDF.index), 'songs in', round((t3-t1), 2), 'seconds.')
-<<<<<<< HEAD
+print('\n','Got', len(songsDF.index), 'songs in', round((t3-t1), 2), 'seconds.')
 
 print('Storing in HDF5...')
 songsDF = convert_byte_data(songsDF)
 songsDF.to_hdf('preprocessing/songs.h5', 'songs')
-=======
->>>>>>> 3de93ddae4ad8209e2b55faa2c029f2b89b9c07e
 # print(songsDF)
