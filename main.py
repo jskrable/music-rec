56a8c33c156496c757937e4165cdd5c058ee7bf5
Jack Skrable <jskrable@gmail.com>
2019-03-29 22:25:13 -0400

    comments on main

--- main.py
+++ main.py
@@ -16,20 +16,16 @@ import read_h5 as read
 import preprocessing as pp
 
 
-# MAIN
-###############################################################################
-
 t_start = time.time()
 songsDF = read.h5_to_df('./MillionSongSubset/data', 200)
 t_extract = time.time()
 print('\nGot', len(songsDF.index), 'songs in', round((t_extract-t_start), 2), 'seconds.')
 
 print('Pre-processing extracted song data...')
-songsDF = pp.convert_byte_data(songsDF)
 t_preproc = time.time()
+songsDF = pp.convert_byte_data(songsDF)
+x, y = pp.vectorize(songsDF, 'metadata_similar_artists')
 print('Cleaned and processed',len(songsDF.index),'rows in',round((t_preproc - t_extract), 2), 'seconds.')
 
-x, y = pp.vectorize(songsDF, 'metadata_similar_artists')
-# print('Storing compiled song dataframe in HDF5...')
-# songsDF.to_hdf('preprocessing/songs.h5', 'songs')
-# print(songsDF)
+
+
