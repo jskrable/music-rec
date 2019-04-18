9d9ef6e64e939be3bf4517015734b8bf2863bfd3
Jack Skrable <jskrable@bu.edu>
2019-04-18 12:08:30 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # usr/bin/sh
 # Bash script to setup environment on remote server
 
-git clone https://github.com/jskrable/music-rec
-cd music-rec
+# git clone https://github.com/jskrable/music-rec
+# cd music-rec
 
 mkdir data && cd data
 wget http://static.echonest.com/millionsongsubset_full.tar.gz
